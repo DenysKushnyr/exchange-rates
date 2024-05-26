8cd5a3da2bf30ec9d29ad46e19b467bef7220d00
Denys Kushnyr <kuuushnirrr@gmail.com>
2024-05-26 17:12:55 +0300

    add Data Access Layer

---
+++ src/DAL/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Subscription> Subscriptions { get; set; }
+    
+    public AppDbContext()
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+        
+        modelBuilder.Entity<Subscription>()
+            .HasIndex(s => new {s.UserId, s.CurrencyCode})
+            .IsUnique();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        
+        optionsBuilder.UseNpgsql(Constants.DevDbString);
+    }
+}
---
+++ src/DAL/Migrations/20240526130726_AddSubscriptions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubscriptions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    CurrencyCode = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscriptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subscriptions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_UserId_CurrencyCode",
+                table: "Subscriptions",
+                columns: new[] { "UserId", "CurrencyCode" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscriptions");
+        }
+    }
+}
